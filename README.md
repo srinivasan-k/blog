6a6f8c44e2174592ff0d9ab25f9332de39308ba9
srinivasan-k <lotus123.2008@hotmail.com>
2013-06-06 19:07:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+== Welcome to Rails
+This is a Exercise done for an blog application on referring the James Malvin book.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3b8aeb7a1a42c0b7801a6dc7cb579d4fb7003867cd68a880f99f2e46c9a348b8ea5f3643745ffa8ed2c420c4a92383a60f317b94acbd215649828c4670aa2882'
